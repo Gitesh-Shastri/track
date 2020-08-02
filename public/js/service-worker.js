e038070f340c4cec124a734b68144a1f824b49bb
Gitesh-Shastri <giteshshastri100@gmail.com>
2020-08-02 20:34:49 +0530

    Added Changes For Api

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -24,5 +24,5 @@ self.addEventListener('install', e => {
 // Call Fetch Event
 self.addEventListener('fetch', e => {
   console.log('Service Worker: Fetching');
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  
 });
