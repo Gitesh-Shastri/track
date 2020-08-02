378debdc46c1834fa775a8671e64379fc7654085
Gitesh-Shastri <giteshshastri100@gmail.com>
2020-08-02 19:48:16 +0530

    Added Chnages

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -2,18 +2,13 @@ console.log('Service Worker Started');
 
 const cache_files = [ 'index.html', '/js/bundle.js' ];
 
-self.addEventListener('install', (e) => {
-	console.log('Registered');
-	e.waitUntil(
-		caches
-			.open('v1')
-			.then((cache) => {
-				console.log('Service Worker Caching Files');
-				cache.addAll(cache_files);
-			})
-			.then(self.skipWaiting())
-	);
-});
+caches
+	.open('v1')
+	.then((cache) => {
+		console.log('Service Worker Caching Files');
+		cache.addAll(cache_files);
+	})
+	.then(self.skipWaiting())	
 
 self.addEventListener('fetch', (e) => {
 	console.log('Service Worker Fetching Files');
