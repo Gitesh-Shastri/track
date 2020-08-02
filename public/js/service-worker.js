27260f8e0d6974c5023a2ce1e4fabbd12e286462
Gitesh-Shastri <giteshshastri100@gmail.com>
2020-08-02 20:32:05 +0530

    Added Changes For Api

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -20,23 +20,6 @@ self.addEventListener('install', e => {
   );
 });
 
-// Call Activate Event
-self.addEventListener('activate', e => {
-  console.log('Service Worker: Activated');
-  // Remove unwanted caches
-  e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== cacheName) {
-            console.log('Service Worker: Clearing Old Cache');
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
 
 // Call Fetch Event
 self.addEventListener('fetch', e => {
